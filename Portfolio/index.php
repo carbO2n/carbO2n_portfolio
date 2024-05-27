9938b242ab42dfa09e1c4c261ba0add90c38e854
carbO2n <159403734+carbO2n@users.noreply.github.com>
2024-05-27 14:03:22 +0800

    Add files via upload

---
+++ Portfolio/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<header class="header">
+        <a href="#" class="logo">Logo</a>
+
+    <nav class="navbar">
+        <a href="#home" class="active">Home</a> 
+        <a href="#about">About</a> 
+        <a href="#gallery">Gallery</a> 
+        <a href="#contact">Contact</a> 
+    </nav>
+</header>
+<div class="container-fluid">
+  <!-- Content here -->
+
+<section id="home" class="translucent-text">Home</section>
+
+
+
+<section id="about" class="translucent-text">
+
+<div class="aboutme">
+    <h1>About</h1>
+    <p>Im a digital artist and traditional artist that is a student of "Bachelor of Science in Information Technology"</p>
+</div>
+<div class="about-image">
+        <img src="images/PRIVYET.png">
+        </div>
+
+</section>
+
+</div>
+
+
+
+<section id="gallery" class="translucent-text">Gallery</section>
+<section id="contact" class="translucent-text">Contact</section>
+
+    <script src="script.js"></script>
+</body>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
